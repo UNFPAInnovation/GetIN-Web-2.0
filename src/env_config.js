d28a84e37ac1dab8b174ff24b14d63190257cef4
innyanzi <innyanzi@outbox.co.ug>
2019-11-25 20:19:36 +0300

    Fix: WWW extension enabled in url env config file

--- src/env_config.js
+++ src/env_config.js
@@ -3,7 +3,7 @@ const serverUrl = require('./config.json');
 
 function env_config(){
     console.log(window.location.hostname)
-    if (window.location.hostname==="dashboard.getinmobile.org"){
+    if (window.location.hostname==="dashboard.getinmobile.org" || window.location.hostname==="www.dashboard.getinmobile.org"){
         return serverUrl.BASE_URL;
     }
     else if(window.location.hostname==="dash.getinmobile.org"){
