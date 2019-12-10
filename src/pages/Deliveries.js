1eefad9899d05a977bd3d20ac40b72acdf5737bd
innyanzi <innyanzi@outbox.co.ug>
2019-12-10 15:58:23 +0300

    Fix: Error to catch empty data on family planning

--- src/pages/Deliveries.js
+++ src/pages/Deliveries.js
@@ -708,11 +708,11 @@ class Home extends Component {
   familyPlanningFormatter(cell, row) {
     // console.log(cell);
     let familyPlanning = "";
-     if(row.family_planning[0].using_family_planning===true){
+     if(row.family_planning && row.family_planning[0].using_family_planning===true){
          return familyPlanning = "Yes, "+row.family_planning[0].method;
      }
     else{
-         return familyPlanning = "None, "+row.family_planning[0].no_family_planning_reason;
+         return familyPlanning = "None, "+row.family_planning && row.family_planning[0].no_family_planning_reason;
      }
    }
   enumFormatter(cell, row, enumObject) {
