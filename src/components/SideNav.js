fb32c0ef9b16ef2b934fa47643cedba8681ee1b7
Ian Nnyanzi <innyanzi@outbox.co.ug>
2021-11-11 08:41:49 +0300

    Removed Users for Role mananger on side nav

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,4 +1,4 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAlignRight,
@@ -10,12 +10,12 @@ import {
   faUsers,
   faHome,
   faEnvelope,
-  faCog
+  faCog,
 } from "@fortawesome/free-solid-svg-icons";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import {GlobalContext} from '../context/GlobalState';
-import Logo from '../assets/images/Logo.png';
+import { GlobalContext } from "../context/GlobalState";
+import Logo from "../assets/images/Logo.png";
 
 export default class SideNav extends Component {
   constructor(props) {
@@ -23,7 +23,7 @@ export default class SideNav extends Component {
     this.state = {
       mini: false,
       show: true,
-      district: ""
+      district: "",
     };
     this.ToggleMenu = this.ToggleMenu.bind(this);
   }
@@ -32,7 +32,7 @@ export default class SideNav extends Component {
     let detailComponent = document.getElementById("detailComponent");
     if (this.state.mini === false) {
       this.setState({
-        mini: true
+        mini: true,
       });
       sideNavBar.classList.remove("col-md-2");
       sideNavBar.classList.add("col-md-1");
@@ -40,7 +40,7 @@ export default class SideNav extends Component {
       detailComponent.classList.add("col-md-11");
     } else {
       this.setState({
-        mini: false
+        mini: false,
       });
       sideNavBar.classList.remove("col-md-1");
       sideNavBar.classList.add("col-md-2");
@@ -53,18 +53,18 @@ export default class SideNav extends Component {
     let stateX = this.state.show;
     if (stateX === false) {
       this.setState({
-        show: true
+        show: true,
       });
     } else {
       this.setState({
-        show: false
+        show: false,
       });
     }
   }
   activeMenu() {
     document.getElementById("girlsLink").classList.remove("active");
     document.getElementById("dashboardLink").classList.remove("active");
-    document.getElementById("usersLink").classList.remove("active");
+    document.getElementById("usersLink")?.classList.remove("active");
     document.getElementById("anc_visitsLink").classList.remove("active");
     document.getElementById("follow_upsLink").classList.remove("active");
     document.getElementById("messagesLink").classList.remove("active");
@@ -77,7 +77,7 @@ export default class SideNav extends Component {
     } else if (window.location.pathname === "/girls") {
       document.getElementById("girlsLink").classList.add("active");
     } else if (window.location.pathname === "/users") {
-      document.getElementById("usersLink").classList.add("active");
+      document.getElementById("usersLink")?.classList.add("active");
     } else if (window.location.pathname === "/anc_visits") {
       document.getElementById("anc_visitsLink").classList.add("active");
     } else if (window.location.pathname === "/follow_ups") {
@@ -88,8 +88,7 @@ export default class SideNav extends Component {
       document.getElementById("healthFacilitiesLink").classList.add("active");
     } else if (window.location.pathname === "/messages") {
       document.getElementById("messagesLink").classList.add("active");
-    }
-     else if (window.location.pathname === "/settings") {
+    } else if (window.location.pathname === "/settings") {
       document.getElementById("settingsLink").classList.add("active");
     }
   }
@@ -100,13 +99,14 @@ export default class SideNav extends Component {
     this.activeMenu();
     let district = sessionStorage.getItem("district");
     this.setState({
-      district: district
+      district: district,
     });
   }
 
   static contextType = GlobalContext;
 
   render() {
+    let role = sessionStorage.getItem("role");
     return (
       <div className={this.state.show ? "sideNav" : "sideNav hiddenMenu"}>
         <nav className="headerNav navbar navbar-default navbar-static-top">
@@ -164,12 +164,14 @@ export default class SideNav extends Component {
                 <FontAwesomeIcon icon={faHospital} />
               </Link>
             </ListGroupItem>
-            <ListGroupItem id="usersLink">
-              <Link to="/users">
-                {this.state.mini === false && "Users"}{" "}
-                <FontAwesomeIcon icon={faUsers} />
-              </Link>
-            </ListGroupItem>
+            {role !== "manager" && (
+              <ListGroupItem id="usersLink">
+                <Link to="/users">
+                  {this.state.mini === false && "Users"}{" "}
+                  <FontAwesomeIcon icon={faUsers} />
+                </Link>
+              </ListGroupItem>
+            )}
             <ListGroupItem id="messagesLink">
               <Link to="/messages">
                 {this.state.mini === false && "Messages"}{" "}
